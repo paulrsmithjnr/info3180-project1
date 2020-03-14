227f0216cc731188474ee192fe15cde0eb76a6ed
Paul Smith <paulrsmithjnr@gmail.com>
2020-03-14 10:51:51 -0500

    Updated Database URI for Heroku Deployment.

--- app/__init__.py
+++ app/__init__.py
@@ -13,10 +13,5 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a w
 
 db = SQLAlchemy(app)
 
-# Flask-Login login manager
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
 app.config.from_object(__name__)
 from app import views
