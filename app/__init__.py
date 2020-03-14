cc56932cd54cd5ab11dc6d1adc1a95d608f1525b
Paul Smith <paulrsmithjnr@gmail.com>
2020-03-14 01:00:16 -0500

    Updated Database URI for Heroku Deployment.

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ UPLOAD_FOLDER = './app/static/uploads'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "random_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://xuqbiiebtirbdm:75937ea6212a88bcb04060b2912557dfb4ecf73933b0805f7cd6aad44999f20d@ec2-35-172-85-250.compute-1.amazonaws.com:5432/demdudd69iu1e8"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
