5d2799ae7c741de6bb40d8d09decdf1dc57a4443
Paul Smith <paulrsmithjnr@gmail.com>
2020-03-14 16:33:02 -0500

    Fixed Heroku Deployment Issue

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ UPLOAD_FOLDER = './app/static/uploads'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "random_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://jrwagwsvveqdyq:d3b01edc599b779de8e660424263867202089a8783719506324e993913c68e69@ec2-23-22-156-110.compute-1.amazonaws.c"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
